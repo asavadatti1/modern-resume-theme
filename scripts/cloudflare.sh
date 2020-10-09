e5f9480c77a21d53ea604f0dfa239bd3c5830cd5
Amrita Savadatti <asavadatti1@gmail.com>
2020-10-08 23:09:14 -0700

    ip

--- scripts/cloudflare.sh
+++ scripts/cloudflare.sh
@@ -2,8 +2,8 @@
 # https://www.cloudflare.com/ips
 # https://support.cloudflare.com/hc/en-us/articles/200169166-How-do-I-whitelist-CloudFlare-s-IP-addresses-in-iptables-
 
-for i in `curl https://www.cloudflare.com/ips-v4`; do iptables -I INPUT -p tcp -m multiport --dports http,https -s $i -j ACCEPT; done
-for i in `curl https://www.cloudflare.com/ips-v6`; do ip6tables -I INPUT -p tcp -m multiport --dports http,https -s $i -j ACCEPT; done
+for i in `curl https://www.cloudflare.com/ips-v4`; do iptables -I INPUT -p tcp -m multiport --dports http,https -s $131.0.72.0/22 -j ACCEPT; done
+for i in `curl https://www.cloudflare.com/ips-v6`; do ip6tables -I INPUT -p tcp -m multiport --dports http,https -s $2400:cb00::/32 -j ACCEPT; done
 
 # Avoid racking up billing/attacks
 # WARNING: If you get attacked and CloudFlare drops you, your site(s) will be unreachable.
